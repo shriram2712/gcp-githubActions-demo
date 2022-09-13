570b7e02a7b02a30a4613079c5cae904bf24e053
samuellee-zy <samuelee.zy@gmail.com>
2022-09-13 12:32:33 +1000

    Added VM Instances

--- main.tf
+++ main.tf
@@ -3,11 +3,6 @@ terraform {
     google = {
       source = "hashicorp/google"
     }
-    /*
-    doormat = {
-      source  = "doormat.hashicorp.services/hashicorp-security/doormat"
-      version = "~> 0.0.2"
-    } */
 
   }
   required_version = ">= 1.1.0"
@@ -21,17 +16,7 @@ terraform {
   }
 }
 
-// provider "doormat" {}
-/*
-data "doormat_gcp_credentials" "creds" {
-  provider = doormat
-
-  service_account = "samuel-lee-dev@hc-ba2320067c904daea31de2e996c.iam.gserviceaccount.com"
-  project_id        = "hc-ba2320067c904daea31de2e996c"
-}*/
-
 provider "google" {
-  //access_token = data.doormat_gcp_credentials.creds.access_token
   project      = "hc-ba2320067c904daea31de2e996c"
   region       = "australia-southeast1"
   zone         = "a"
@@ -39,5 +24,23 @@ provider "google" {
 
 resource "google_compute_network" "vpc_network" {
   name = "sam-vpc-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "sam-terraform-instance"
+  machine_type = "f1-micro"
+  tags = ["ghAction", "demo"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
 }
 
