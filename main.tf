bac00355a984ddb820f7cf6ae1f1bf0e439ecc05
samuellee-zy <samuelee.zy@gmail.com>
2022-09-13 12:24:20 +1000

    Updated credentials

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ terraform {
     google = {
       source = "hashicorp/google"
     }
-
+    /*
     doormat = {
       source  = "doormat.hashicorp.services/hashicorp-security/doormat"
       version = "~> 0.0.2"
-    }
+    } */
 
   }
   required_version = ">= 1.1.0"
@@ -22,13 +22,13 @@ terraform {
 }
 
 provider "doormat" {}
-
+/*
 data "doormat_gcp_credentials" "creds" {
   provider = doormat
 
   service_account = "samuel-lee-dev@hc-ba2320067c904daea31de2e996c.iam.gserviceaccount.com"
   project_id        = "hc-ba2320067c904daea31de2e996c"
-}
+}*/
 
 provider "google" {
   access_token = data.doormat_gcp_credentials.creds.access_token
@@ -38,6 +38,6 @@ provider "google" {
 }
 
 resource "google_compute_network" "vpc_network" {
-  name = "sam-gpc-vpc-network"
+  name = "sam-vpc-network"
 }
 
